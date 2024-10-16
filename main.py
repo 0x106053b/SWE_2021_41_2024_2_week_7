893cb57836111e322c827abdbe7d8507e79a3f1e
0x106053b <1000daughter@g.skku.edu>
2024-10-17 02:17:50 +0900

    week7 assignment 2020312491

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import List
 def twoSum(nums : List[int], target : int) -> List[int] :
+    answer = []
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
-                print([i, j])
-                return [i, j]
+                answer += [i, j]
+                break
+    return answer
